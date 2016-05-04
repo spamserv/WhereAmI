1e2c943dac6c6e0088b54d58edfa1d93cb96ac69
Josip Vojak <josipvojak@gmail.com>
2016-05-04 10:38:42 +0200

    Removed unnecessary packages

--- app/src/main/java/hr/etfos/josipvojak/whereami/LocationActivity.java
+++ app/src/main/java/hr/etfos/josipvojak/whereami/LocationActivity.java
@@ -15,11 +15,7 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
